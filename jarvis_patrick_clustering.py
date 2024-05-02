66d2144ca866cd33c1c3ac19737bc7e870494ebb
JayakrishnasaiChintagumpula <146905031+JayakrishnasaiChintagumpula@users.noreply.github.com>
2024-05-02 19:17:07 -0400

    Update jarvis_patrick_clustering.py

--- jarvis_patrick_clustering.py
+++ jarvis_patrick_clustering.py
@@ -8,7 +8,6 @@ import numpy as np
 from numpy.typing import NDArray
 import pickle
 from scipy.spatial.distance import cdist
-import matplotlib.backends.backend_pdf as pdf
 from sklearn.cluster import KMeans
 from sklearn.metrics import adjusted_rand_score
 from matplotlib.backends.backend_pdf import PdfPages
